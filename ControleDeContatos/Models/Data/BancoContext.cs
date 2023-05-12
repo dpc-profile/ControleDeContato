901cce9d5fb732a530f441da815a7bd2747f30ea
dpc-profile <daniel.pcastro11@gmail.com>
2023-05-12 22:50:14 +0000

    Usando db_teste apenas ao rodar testes

--- ControleDeContatos/Models/Data/BancoContext.cs
+++ ControleDeContatos/Models/Data/BancoContext.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 
 using ControleDeContatos.Models;
 
@@ -12,10 +13,21 @@ namespace ControleDeContatos.Data
 
         public DbSet<ContatoModel> Contatos { get; set; }
 
+        // Detecta se está rodando pelo comando dotnet test
+        public static readonly bool IsRunningFromNUnit = AppDomain
+                .CurrentDomain.GetAssemblies()
+                .Any(a => a.FullName.ToLowerInvariant().StartsWith("xunit"));
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            // string stringconexao = "server=mysql_db;database=db;user=user;password=passUser";
-            string stringconexao = "server=mysql_db;database=db_teste;user=user;password=passUser";
+            string stringconexao = "server=mysql_db;database=db;user=user;password=passUser";
+            
+            if (IsRunningFromNUnit)
+            {
+                stringconexao = "server=mysql_db;database=db_teste;user=user;password=passUser";
+
+            }
+            
             optionsBuilder.UseMySql(stringconexao, ServerVersion.AutoDetect(stringconexao));
         }
     }
