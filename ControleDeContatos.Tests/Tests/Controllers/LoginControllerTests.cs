3660123c98294a3b92d938e5bc4c970583908aba
dpc-profile <daniel.pcastro11@gmail.com>
2023-06-01 21:49:05 +0000

    feito Exception_Generico

--- ControleDeContatos.Tests/Tests/Controllers/LoginControllerTests.cs
+++ ControleDeContatos.Tests/Tests/Controllers/LoginControllerTests.cs
@@ -187,7 +187,84 @@ namespace ControleDeContatos.Tests.Tests.Controllers
             Assert.Equal("Index", redirectToActionResult.ActionName);
         }
 
+        [Fact]
+        public void TestarEntrar_Exception_SenhaInvalido()
+        {
+            // Arrange
+            DefaultHttpContext httpContext = new DefaultHttpContext();
+            TempDataDictionary tempData = new TempDataDictionary(httpContext, Mock.Of<ITempDataProvider>());
 
+            Mock<IUsuarioServices> mockUsuarioServices = new Mock<IUsuarioServices>();
+            Mock<ILoginServices> mockLoginServices = new Mock<ILoginServices>();
+            Mock<ISessao> mockSessao = new Mock<ISessao>();
+            // Faz setup buscando uma sessão e retornando o usuarioModel
+            mockSessao.Setup(s => s.BuscarSessaoUsuario());
+            mockLoginServices.Setup(s => s.FazerLogin(It.IsAny<LoginModel>(), It.IsAny<ISessao>()))
+                             .Throws(new SenhaInvalidaException("Senha inválida"));
+
+            var controller = new LoginController(
+                mockUsuarioServices.Object,
+                mockSessao.Object,
+                mockLoginServices.Object)
+            { TempData = tempData };
+
+            // Act
+            // Adiciona um model com um erro falso
+            var result = controller.Entrar(It.IsAny<LoginModel>());
+
+            // Assert
+            // Verifica se a tempData deu sucesso
+            Assert.True(controller.TempData.ContainsKey("MensagemErro"));
+            // Verifica se a mensagem é de erro
+            Assert.True(controller.TempData.Values.Contains("Senha inválida"));
+            // Assert.Matches("Ops, não conseguimos apagar o contato", controller.TempData["MensagemErro"].ToString()); 
+            // Verifica se o retorno é RedirectToAction
+            var redirectToActionResult = Assert.IsType<RedirectToActionResult>(result);
+            // Verifica se a controller é nula
+            Assert.Null(redirectToActionResult.ControllerName);
+            // Verifica se foi redirecionado para Index do controller
+            Assert.Equal("Index", redirectToActionResult.ActionName);
+        }
+
+        [Fact]
+        public void TestarEntrar_Exception_Generico()
+        {
+            // Arrange
+            DefaultHttpContext httpContext = new DefaultHttpContext();
+            TempDataDictionary tempData = new TempDataDictionary(httpContext, Mock.Of<ITempDataProvider>());
+
+            Mock<IUsuarioServices> mockUsuarioServices = new Mock<IUsuarioServices>();
+            Mock<ILoginServices> mockLoginServices = new Mock<ILoginServices>();
+            Mock<ISessao> mockSessao = new Mock<ISessao>();
+            // Faz setup buscando uma sessão e retornando o usuarioModel
+            mockSessao.Setup(s => s.BuscarSessaoUsuario());
+            mockLoginServices.Setup(s => s.FazerLogin(It.IsAny<LoginModel>(), It.IsAny<ISessao>()))
+                             .Throws(new Exception());
+
+            var controller = new LoginController(
+                mockUsuarioServices.Object,
+                mockSessao.Object,
+                mockLoginServices.Object)
+            { TempData = tempData };
+
+            // Act
+            // Adiciona um model com um erro falso
+            var result = controller.Entrar(It.IsAny<LoginModel>());
+
+            // Assert
+            // Verifica se a tempData deu sucesso
+            Assert.True(controller.TempData.ContainsKey("MensagemErro"));
+            // Verifica se a mensagem é de erro
+            Assert.Matches("Ops, não foi possivel fazer o login", controller.TempData["MensagemErro"].ToString()); 
+            // Verifica se o retorno é RedirectToAction
+            var redirectToActionResult = Assert.IsType<RedirectToActionResult>(result);
+            // Verifica se a controller é nula
+            Assert.Null(redirectToActionResult.ControllerName);
+            // Verifica se foi redirecionado para Index do controller
+            Assert.Equal("Index", redirectToActionResult.ActionName);
+        }
+
+        
 
     }
 }
