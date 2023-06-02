712004920afbc3efb99da3d6e9b23673c937d80f
dpc-profile <daniel.pcastro11@gmail.com>
2023-06-02 14:35:32 +0000

    feito TestarFazerLogin

--- ControleDeContatos.Tests/Tests/Services/LoginServicesTests.cs
+++ ControleDeContatos.Tests/Tests/Services/LoginServicesTests.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using ControleDeContatos.Models;
 using ControleDeContatos.Repository;
 using ControleDeContatos.Services;
+using ControleDeContatos.Services.Interfaces;
 using ControleDeContatos.Tests.Tests.Controllers;
 
 using Moq;
@@ -16,6 +17,26 @@ namespace ControleDeContatos.Tests.Tests.Services
 {
     public class LoginServicesTests
     {
-        
+        [Fact]
+        public void TestarFazerLogin()
+        {
+            // Arrange
+            Mock<IUsuarioRepository> mockRepo = new Mock<IUsuarioRepository>();
+            Mock<IEmail> mockEmail = new Mock<IEmail>();
+            Mock<ISessao> mockSessao = new Mock<ISessao>();
+
+            mockRepo.Setup(s => s.BuscarPorLogin(It.IsAny<string>()))
+                    .Returns(fakeUsuario.ModeloDadosUsuario());
+            
+            mockSessao.Setup(s => s.BuscarSessaoUsuario())
+                      .Returns(fakeUsuario.ModeloDadosUsuario());
+
+            var services = new LoginServices(mockRepo.Object, mockEmail.Object);
+
+
+            // Act
+            services.FazerLogin(fakeUsuario.ModeloLoginValido(), mockSessao.Object);
+
+        }
     }
 }
