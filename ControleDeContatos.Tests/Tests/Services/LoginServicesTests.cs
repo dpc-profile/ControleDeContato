a474400146d6164f2347ad9d75c996ab37938678
dpc-profile <daniel.pcastro11@gmail.com>
2023-06-02 14:42:28 +0000

    feito SenhaInvalida

--- ControleDeContatos.Tests/Tests/Services/LoginServicesTests.cs
+++ ControleDeContatos.Tests/Tests/Services/LoginServicesTests.cs
@@ -63,5 +63,30 @@ namespace ControleDeContatos.Tests.Tests.Services
             Assert.Equal("Usuário inválido", mensagem.Message);
 
         }
+        [Fact]
+        public void TestarFazerLogin_Exception_SenhaInvalida()
+        {
+            // Arrange
+            Mock<IUsuarioRepository> mockRepo = new Mock<IUsuarioRepository>();
+            Mock<IEmail> mockEmail = new Mock<IEmail>();
+            Mock<ISessao> mockSessao = new Mock<ISessao>();
+
+            mockRepo.Setup(s => s.BuscarPorLogin(It.IsAny<string>()))
+                    .Returns(fakeUsuario.ModeloDadosUsuario_SenhaInvalida());
+            
+            mockSessao.Setup(s => s.BuscarSessaoUsuario())
+                      .Returns(fakeUsuario.ModeloDadosUsuario());
+
+            var services = new LoginServices(mockRepo.Object, mockEmail.Object);
+
+
+            // Assert
+            var mensagem = Assert.Throws<SenhaInvalidaException>(
+                // Act
+                () => services.FazerLogin(fakeUsuario.ModeloLoginValido(), mockSessao.Object)
+            );
+            Assert.Equal("Senha inválida", mensagem.Message);
+
+        }
     }
 }
