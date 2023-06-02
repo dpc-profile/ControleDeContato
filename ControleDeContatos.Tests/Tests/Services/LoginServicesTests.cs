12d0ed9a181ed806e9bdde62590fd23cd0cf5635
dpc-profile <daniel.pcastro11@gmail.com>
2023-06-02 14:56:24 +0000

    feito Exception_LoginNaoEncontrado

--- ControleDeContatos.Tests/Tests/Services/LoginServicesTests.cs
+++ ControleDeContatos.Tests/Tests/Services/LoginServicesTests.cs
@@ -88,7 +88,6 @@ namespace ControleDeContatos.Tests.Tests.Services
             Assert.Equal("Senha inválida", mensagem.Message);
 
         }
-
         [Fact]
         public void TestarValidaUsuarioCadastrado()
         {
@@ -103,11 +102,57 @@ namespace ControleDeContatos.Tests.Tests.Services
 
 
             // Act
-            var result = services.ValidaUsuarioCadastrado(It.IsAny<string>(),
-                                                          fakeUsuario.ModeloDadosUsuario().Login);
+            var result = services.ValidaUsuarioCadastrado(
+                    It.IsAny<string>(),
+                    fakeUsuario.ModeloDadosUsuario().Login);
 
             // Assert
             Assert.IsType<UsuarioModel>(result);
+        }
+        [Fact]
+        public void TestarValidaUsuarioCadastrado_Exception_EmailNaoEncontrado()
+        {
+            // Arrange
+            Mock<IUsuarioRepository> mockRepo = new Mock<IUsuarioRepository>();
+            Mock<IEmail> mockEmail = new Mock<IEmail>();
+
+            mockRepo.Setup(s => s.BuscarPorEmail(It.IsAny<string>()));
+
+            var services = new LoginServices(mockRepo.Object, mockEmail.Object);
+
+
+            // Assert
+            var mensagem = Assert.Throws<EmailNaoEncontradoException>(
+                // Act
+                () => services.ValidaUsuarioCadastrado(
+                    It.IsAny<string>(),
+                    fakeUsuario.ModeloDadosUsuario().Login)
+            );
+
+            Assert.Equal("O email informado não foi encontrado", mensagem.Message);
+        }
+        [Fact]
+        public void TestarValidaUsuarioCadastrado_Exception_LoginNaoEncontrado()
+        {
+            // Arrange
+            Mock<IUsuarioRepository> mockRepo = new Mock<IUsuarioRepository>();
+            Mock<IEmail> mockEmail = new Mock<IEmail>();
+
+            mockRepo.Setup(s => s.BuscarPorEmail(It.IsAny<string>()))
+                    .Returns(fakeUsuario.ModeloDadosUsuario());
+
+            var services = new LoginServices(mockRepo.Object, mockEmail.Object);
+
+
+            // Assert
+            var mensagem = Assert.Throws<LoginNaoEncontradoException>(
+                // Act
+                () => services.ValidaUsuarioCadastrado(
+                    It.IsAny<string>(),
+                    It.IsAny<string>())
+            );
+
+            Assert.Equal("O login informado não foi encontrado", mensagem.Message);
         }
     }
 }
